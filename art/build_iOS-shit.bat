a1a213c82be3d49d6c891045fae0c583d9c9ca52
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-09 13:18:27 +0200

    Update build_iOS-shit.bat

--- art/build_iOS-shit.bat
+++ art/build_iOS-shit.bat
@@ -2,9 +2,9 @@
 color 0a
 cd ..
 echo BUILDING GAME
-lime build android -D NO_PRECOMPILED_HEADERS
+lime build ios -D NO_PRECOMPILED_HEADERS
 echo.
 echo done.
 pause
 pwd
-explorer.exe export\release\android\bin
+explorer.exe export/release/ios/build
