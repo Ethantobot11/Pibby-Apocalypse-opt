eac53382139a2bb537b85f64caf31a41ac1fcd3e
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-09 13:20:05 +0200

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -2,3 +2,12 @@
 import Paths;
 import Highscore;
 #end
+#if android
+import android.content.Context as AndroidContext;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+#end
