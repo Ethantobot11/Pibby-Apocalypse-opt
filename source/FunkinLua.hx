496b814e5892a12a18c14c73c6821a902d9dcd7b
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-10-03 17:46:14 +0200

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -2897,23 +2897,6 @@ class FunkinLua {
 			else
 				PlayState.instance.addLuaTouchPad();
 		});
-		}
-
-		public static function insertLuaTpad(?pos:Int = -1)
-	    {
-		    if(instance != null)
-		    {
-			    var tagObject:FlxObject = PlayState.instance.luaTouchPad;
-
-			    if(tagObject != null)
-			{
-				   if(pos < 0) instance.add(tagObject);
-				   else instance.insert(pos, tagObject);
-				   return true;
-			 }
-		 }
-		 return false;
-	    }
 
 		Lua_helper.add_callback(lua, "touchJustPressed", TouchUtil.justPressed);
 			return TouchUtil.overlapsComplex(obj, cam);
@@ -2983,6 +2966,23 @@ class FunkinLua {
 		}
 		return false;
 	}
+}
+
+	 public static function insertLuaTpad(?pos:Int = -1)
+	 {
+		    if(instance != null)
+		    {
+			    var tagObject:FlxObject = PlayState.instance.luaTouchPad;
+
+			    if(tagObject != null)
+			{
+				   if(pos < 0) instance.add(tagObject);
+				   else instance.insert(pos, tagObject);
+				   return true;
+			 }
+		 }
+		 return false;
+	 }
 }
 
 	#if hscript
